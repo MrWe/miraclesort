45455ddf4e17c4fde7dc33d69d9d32a5c16799f0
Erik Wiker <Wiker@dhcp-037155.wlan.ntnu.no>
2015-08-06 12:57:11 +0200

    Clean beginnings

--- README.md
+++
@@ -1,4 +0,0 @@
-miraclesort
-===========
-
-general fun stuff
--- js/main.js
+++
@@ -1,7 +0,0 @@
-var hr = document.getElementById("hr").addEventListener('load', function(e) {
-	setTimeout(function(){
-			hr.style.width
-	}10);
-
-
-});
